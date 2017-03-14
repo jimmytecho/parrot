e1ed4bbfee6ca4664ead89e5700ec264bfde9f5b
jimmytecho <marrch30@gmail.com>
2017-03-14 15:05:18 -0700

    will this work?

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Process application/json
 app.use(bodyParser.json())
@@ -29,7 +29,7 @@ app.get('/webhook/', function (req, res) {
 })
 
 // Spin up the server
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('running on port', app.get('port'))
 })
 
@@ -37,7 +37,7 @@ app.listen(app.get('port'), function() {
 // API End Point - added by Stefan
 
 var input
-
+var final
 app.post('/webhook/', function (req, res) {
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
@@ -45,15 +45,28 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id
         if (event.message && event.message.text) {
             text = event.message.text
+            input = text
             if (text === 'hi') {
                 sendGenericMessage(sender)
-                continue    
+                continue
             }
-            sendTextMessage(sender, text.substring(0, 200))   //< "parrot: " + >was before text.substring 
-            sendTranslation(sender, text.substring(0, 200))
+            translate({
+                text: input,
+                source: 'en',
+                target: 'fr'
+            }, function (result) {
+                console.log(result);
+                final = String(result);
+            });
+        //  sendTextMessage(sender, text.substring(0, 200))   //< "parrot: " + >was before text.substring 
+            setTimeout(function()){
+            sendTranslation(sender, text.substring(0, 200));
+            },5000);
+
+        }
         if (event.postback) {
             text = JSON.stringify(event.postback)
-            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+            sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
             continue
         }
     }
@@ -62,25 +75,22 @@ app.post('/webhook/', function (req, res) {
 
 var token = "EAAaVxKEKRM4BAA0Sco3v9D8gYghtzqRehtYJ3zE0SYnOEVOtXbjDJzRqs4EbmLIRXnAxT8KRZA4vRZAI2cBE0joKkOOjiOZBwKu28XWTrWcRkulGWkzH5g4e5PUphZBddZBzeaKBZCGm9wpxrIfV8BZBWfX6cHwYZAvV7Ml42O0rCAZDZD"
 
-var final
-
-
 //all messages
 
 // function to echo back messages - added by Stefan
 function sendTextMessage(sender, text) {
     messageData = {
-        text:text
+        text: text
     }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: { access_token: token },
         method: 'POST',
         json: {
-            recipient: {id:sender},
+            recipient: { id: sender },
             message: messageData,
         }
-    }, function(error, response, body) {
+    }, function (error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
@@ -89,27 +99,20 @@ function sendTextMessage(sender, text) {
     })
 }
 
+// place for final translation
 function sendTranslation(sender, input) {
-    translate({
-        text: input,
-        source: 'en',
-        target: 'fr'
-    }, function (result) {
-        console.log(result);
-        final = String(result);
-    });
     messageData = {
-        text:final
+        text: final
     }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: { access_token: token },
         method: 'POST',
         json: {
-            recipient: {id:sender},
+            recipient: { id: sender },
             message: messageData,
         }
-    }, function(error, response, body) {
+    }, function (error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
@@ -117,9 +120,6 @@ function sendTranslation(sender, input) {
         }
     })
 }
-
-
-
 
 // Send an test message back as two cards.
 
@@ -138,10 +138,10 @@ function sendGenericMessage(sender) {
                         "url": "https://www.facebook.com/groups/aichatbots/",
                         "title": "FB Chatbot Group"
                     }, {
-                        "type": "web_url", 
+                        "type": "web_url",
                         "url": "https://www.reddit.com/r/Chat_Bots/",
                         "title": "Chatbots on Reddit"
-                    },{
+                    }, {
                         "type": "web_url",                           //or postback , "title":"anything", "payload":"what you want to say"
                         "url": "https://twitter.com/aichatbots",
                         "title": "Chatbots on Twitter"
@@ -154,7 +154,7 @@ function sendGenericMessage(sender) {
                         "type": "postback",
                         "title": "What's the benefit?",
                         "payload": "Chatbots make content interactive instead of static",
-                    },{
+                    }, {
                         "type": "postback",
                         "title": "What can Chatbots do",
                         "payload": "One day Chatbots will control the Internet of Things! You will be able to control your homes temperature with a text",
@@ -163,7 +163,7 @@ function sendGenericMessage(sender) {
                         "title": "The Future",
                         "payload": "Chatbots are fun! One day your BFF might be a Chatbot",
                     }],
-                },  {
+                }, {
                     "title": "Learning More",
                     "subtitle": "Aking the Deep Questions",
                     "image_url": "http://www.brandknewmag.com/wp-content/uploads/2015/12/cortana.jpg",
@@ -171,7 +171,7 @@ function sendGenericMessage(sender) {
                         "type": "postback",
                         "title": "AIML",
                         "payload": "Checkout Artificial Intelligence Mark Up Language. Its easier than you think!",
-                    },{
+                    }, {
                         "type": "postback",
                         "title": "Machine Learning",
                         "payload": "Use python to teach your maching in 16D space in 15min",
@@ -180,19 +180,19 @@ function sendGenericMessage(sender) {
                         "title": "Communities",
                         "payload": "Online communities & Meetups are the best way to stay ahead of the curve!",
                     }],
-                }]  
-            } 
+                }]
+            }
         }
     }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: { access_token: token },
         method: 'POST',
         json: {
-            recipient: {id:sender},
+            recipient: { id: sender },
             message: messageData,
         }
-    }, function(error, response, body) {
+    }, function (error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
@@ -200,4 +200,3 @@ function sendGenericMessage(sender) {
         }
     })
 }
-
