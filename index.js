714fabc8a79089b5eddcd8610efa9548afece6bf
jimmytecho <marrch30@gmail.com>
2017-03-13 19:52:18 -0700

    revive!

--- index.js
+++ index.js
@@ -77,6 +77,11 @@ request('https://translate.google.com/#en/fr/dream', function (error, response, 
         // Finally, we'll define the variables we're going to capture
         var result;
         // We'll use the unique header class as a starting point.
+
+        result = $('#result_box').chrildren().html()
+        Answer = String(result)
+        /*
+        failed
         $('#result_box').filter(function () {
             // Let's store the data we filter into a variable so we can easily see what's going on.
             var data = $(this);
@@ -88,6 +93,7 @@ request('https://translate.google.com/#en/fr/dream', function (error, response, 
             Answer = String(result)
             
         })
+        */
     }
 })
 
