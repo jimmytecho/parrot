459e4104b380a52071c4fd65eee20aa3ae60e4d1
jimmytecho <marrch30@gmail.com>
2017-03-23 21:09:56 -0700

    new page

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ var request = require('request')
 var translate = require('node-google-translate-skidz');
 var app = express()
 
-//var jsdom = require('jsdom')
-//var url = require('url')
-//var app = module.exports = express.createServer();
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -39,6 +36,42 @@ app.listen(app.get('port'), function () {
 var input
 var final
 var start
+
+
+/*
+mission impossible: personalize settings. 
+curl -X GET "https://graph.facebook.com/v2.6/<USER_ID>?fields=first_name,last_name&access_token=PAGE_ACCESS_TOKEN"  
+·|¦^À³Json ¦r¦ê 
+"first_name" : "Peter"
+"last_name" : "Chang"
+
+*/
+
+/*
+possible solution
+use json!
+if (text === 'xxchangexx')
+record_setting(){
+    request person name?
+    write file: firstname last name, source, target
+}
+
+readID() = request id
+
+source = 'en'
+target = 'fr'
+if (name_recognized(first name last name)){
+    getIDbyname(first name last name)
+    source = ID.source
+    target = ID.target
+}
+translat
+source: source
+target: target...
+}
+
+*/
+
 app.post('/webhook/', function (req, res) {
     
     messaging_events = req.body.entry[0].messaging
@@ -48,10 +81,6 @@ app.post('/webhook/', function (req, res) {
         if (event.message && event.message.text) {
             text = event.message.text
             input = text
-            if (text === 'hi') {
-                sendGenericMessage(sender)
-                continue
-            }
             translate({
                 text: input,
                 source: 'en',
@@ -64,9 +93,6 @@ app.post('/webhook/', function (req, res) {
             setTimeout(function () {
                 clearTimeout(start);
             }, 4000)
-        //  sendTextMessage(sender, text.substring(0, 200))   //< "parrot: " + >was before text.substring 
-         
-            
         }
         if (event.postback) {
             text = JSON.stringify(event.postback)
@@ -85,35 +111,8 @@ function startprocess() {
     }, 3000);
 }
 
-
-
-
-
 //all messages
 
-// function to echo back messages - added by Stefan
-function sendTextMessage(sender, text) {
-    messageData = {
-        text: text
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { id: sender },
-            message: messageData,
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-// place for final translation
 function sendTranslation(sender, input) {
     messageData = {
         text: final
@@ -135,82 +134,3 @@ function sendTranslation(sender, input) {
     })
 }
 
-// Send an test message back as two cards.
-
-function sendGenericMessage(sender) {
-    messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Ai Chat Bot Communities",
-                    "subtitle": "Communities to Follow",
-                    "image_url": "http://1u88jj3r4db2x4txp44yqfj1.wpengine.netdna-cdn.com/wp-content/uploads/2016/04/chatbot-930x659.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.facebook.com/groups/aichatbots/",
-                        "title": "FB Chatbot Group"
-                    }, {
-                        "type": "web_url",
-                        "url": "https://www.reddit.com/r/Chat_Bots/",
-                        "title": "Chatbots on Reddit"
-                    }, {
-                        "type": "web_url",                           //or postback , "title":"anything", "payload":"what you want to say"
-                        "url": "https://twitter.com/aichatbots",
-                        "title": "Chatbots on Twitter"
-                    }],
-                }, {
-                    "title": "Chatbots FAQ",
-                    "subtitle": "Aking the Deep Questions",
-                    "image_url": "https://tctechcrunch2011.files.wordpress.com/2016/04/facebook-chatbots.png?w=738",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "What's the benefit?",
-                        "payload": "Chatbots make content interactive instead of static",
-                    }, {
-                        "type": "postback",
-                        "title": "What can Chatbots do",
-                        "payload": "One day Chatbots will control the Internet of Things! You will be able to control your homes temperature with a text",
-                    }, {
-                        "type": "postback",
-                        "title": "The Future",
-                        "payload": "Chatbots are fun! One day your BFF might be a Chatbot",
-                    }],
-                }, {
-                    "title": "Learning More",
-                    "subtitle": "Aking the Deep Questions",
-                    "image_url": "http://www.brandknewmag.com/wp-content/uploads/2015/12/cortana.jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "AIML",
-                        "payload": "Checkout Artificial Intelligence Mark Up Language. Its easier than you think!",
-                    }, {
-                        "type": "postback",
-                        "title": "Machine Learning",
-                        "payload": "Use python to teach your maching in 16D space in 15min",
-                    }, {
-                        "type": "postback",
-                        "title": "Communities",
-                        "payload": "Online communities & Meetups are the best way to stay ahead of the curve!",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { id: sender },
-            message: messageData,
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
